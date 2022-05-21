d25198ff8a35cd4eb8d514a72ab778643abc8fd3
siskal-man <block-code@mail.ru>
2022-05-21 10:13:26 +0300

    simple crup app

---
+++ database/migrations/2022_05_21_061645_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name'); // Add
+            $table->string('last_name'); // Add
+            $table->integer('age'); // Add
+            $table->string('email'); // Add
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends("layouts.master")
+
+@section('pageTitle', 'Home')
+
+@section('content')
+
+    <div class="jumbotron">
+        <h1 class="display-4">Hello!</h1>
+        <p class="lead">Welcome to my CRUD App.</p>
+    </div>
+
+@endsection
